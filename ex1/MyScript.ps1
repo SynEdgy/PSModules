a4720df00251819729d1c039be81c285f0c8c2b8
gaelcolas <gaelcolas@synedgy.com>
2021-09-20 14:48:27 +0200

    adding q1

---
+++ ex1/MyScript.ps1
@@ -0,0 +1,7 @@
+[datetime] $EventDate = '20/09/2021'
+[string]   $Presenter = 'Gael'
+[string]   $Subject   = 'how to work with PowerShell modules'
+
+# When I run this file, if the event date is today, it prints:
+# Today the 20/09/2021, Gael shows "how to work with PowerShell modules!
+# if it's not the date, print: It's not today!
